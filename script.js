356f1d7d0019c07a10607d7a9b97825cf3aa4719
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-05-24 18:21:39 +0300

    fixed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const field = document.querySelector('.field');
 const ball = document.querySelector('.ball');
 
-field.addEventListener('click', function (e) {
+field.addEventListener('click', (event) => {
 
     const fieldRectObject = field.getBoundingClientRect();
     const {top: fieldRectTop, left: fieldRectLeft} = fieldRectObject;
@@ -9,27 +9,27 @@ field.addEventListener('click', function (e) {
            clientLeft: fieldClientLeft,
            clientHeight: fieldHeight,
            clientWidth: fieldWidth} = field;
-    const {clientX: clickClientX, clientY: clickClientY} = e;
+    const {clientX: clickClientX, clientY: clickClientY} = event;
     const {clientWidth: ballWidth, clientHeight: ballHeight} = ball;
 
     const fieldInnerTop = fieldRectTop + fieldClientTop;
     const fieldInnerLeft = fieldRectLeft + fieldClientLeft;
 
-    let bollTop = clickClientY - ballHeight / 2 - fieldInnerTop;
+    let ballTop = clickClientY - ballHeight / 2 - fieldInnerTop;
     let ballLeft = clickClientX - ballWidth / 2 - fieldInnerLeft;
 
-    if (bollTop < 0) bollTop = 0;
+    if (ballTop < 0) ballTop = 0;
     if (ballLeft < 0) ballLeft = 0;
 
     if (ballLeft + ballWidth > fieldWidth) {
         ballLeft = fieldWidth - ballWidth;
     }
-    if (bollTop + ballHeight > fieldHeight) {
-        bollTop = fieldHeight - ballHeight;
+    if (ballTop + ballHeight > fieldHeight) {
+        ballTop = fieldHeight - ballHeight;
     }
 
     ball.style.left = `${ballLeft}px`;
-    ball.style.top = `${bollTop}px`;
+    ball.style.top = `${ballTop}px`;
 
 });
 
